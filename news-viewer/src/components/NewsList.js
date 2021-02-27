b835825d33dd40624e03e4656f9aff3c7d697b65
dongtony12 <dongtony0523@naver.com>
2021-02-27 20:13:08 +0900

    news viewer

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
 box-sizing:border-box;
@@ -15,23 +17,35 @@ margin-top: 2rem;
 }
 `;
 
-const sampleArticle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160',
-};
+const NewsList = ({category}) =>{
+    
+    const [loading,response,error] = usePromise(()=>{
+        const query = category === 'all' ? '' : `&category = ${category}`;
+        return axios.get (
+            `http://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=4b42b6c4f9ce4409b71d1e4b33b8e628`
+        );
+    },[category]);
+    //대기 중일때
 
-const NewsList = () => {
+    if(loading) {
+        return <NewsListBlock>대기중 ...</NewsListBlock>;
+    }
+    // 아직 article 값이 설정 안됫을때
+    if(!response){
+        return null;
+    }
+
+    if(error){
+        return <NewsListBlock>에러 발생!</NewsListBlock>
+    }
+
+    //article 값이 유효할 때
+    const {articles} = response.data;
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-
+            {articles.map(article=>(
+            <NewsItem key={article.url} article = {article}/>
+            ))}
         </NewsListBlock>
     );
 };
